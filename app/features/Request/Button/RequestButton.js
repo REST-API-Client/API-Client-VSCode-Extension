551bf1a03af218eb2002a596a20aba495fb0e481
unjinjang <alpsboy1996@gmail.com>
2022-07-05 17:06:43 +0900

    [FEAT]: Add request button

---
+++ app/features/Request/Button/RequestButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Button from "../../../components/Button";
+
+function RequestButton() {
+  return <Button buttonPurpose="submit">Send</Button>;
+}
+
+export default RequestButton;
