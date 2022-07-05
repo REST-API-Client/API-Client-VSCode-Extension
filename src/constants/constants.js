762c90fd36d71ab8ef2bfdf8e79b1bb183c32528
unjinjang <alpsboy1996@gmail.com>
2022-07-05 16:45:32 +0900

    [REFACTOR]: Change VSCode Extension host constant variable order

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-const WELCOME_MESSAGE = "Welcome to REST API Tester 😊";
 const MAIN_PANEL_NAME = "REST API Tester";
+const WELCOME_MESSAGE = "Welcome to REST API Tester 😊";
 
 module.exports = { WELCOME_MESSAGE, MAIN_PANEL_NAME };
