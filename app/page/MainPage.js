ca5f7a96b9bba59979694b5a8a8633270e8ba853
unjinjang <alpsboy1996@gmail.com>
2022-07-07 19:34:36 +0900

    [FEAT]: Add MainPage component to render vscode extension main page

---
+++ app/page/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import RequestPanel from "../features/Request/Panel/RequestPanel";
+import ResizeBar from "../features/ResizeBar/ResizeBar";
+
+function MainPage() {
+  return (
+    <>
+      <RequestPanel />
+      <ResizeBar />
+    </>
+  );
+}
+
+export default MainPage;
