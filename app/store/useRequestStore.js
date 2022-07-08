996987af49fd09f8d6f0412f3b08509d51b01cad
unjinjang <alpsboy1996@gmail.com>
2022-07-08 09:55:59 +0900

    [FEAT]: Add global state to keep authorization state

--- app/store/useRequestStore.js
+++ app/store/useRequestStore.js
@@ -1,28 +1,58 @@
 import create from "zustand";
 
-import { GET, PARAMS } from "../constants/request";
+import { GET, NO_AUTH, PARAMS } from "../constants/request";
 
 const initialState = {
   requestMethod: GET,
   requestUrl: "",
   requestUrlParams: "",
   requestOption: PARAMS,
+  authOption: NO_AUTH,
+  authData: { username: "", password: "", token: "" },
 };
 
 const useRequestStore = create((set) => ({
-  requestMethod: initialState.requestMethod,
   requestUrl: initialState.requestUrl,
+  requestMethod: initialState.requestMethod,
   requestUrlParams: initialState.requestUrlParams,
   requestOption: initialState.requestOption,
-
-  handleRequestMethodChange: (method) => set(() => ({ requestMethod: method })),
+  authOption: initialState.authOption,
+  authData: initialState.authData,
 
   handleRequestUrlChange: (url) => set(() => ({ requestUrl: url })),
+
+  handleRequestMethodChange: (method) => set(() => ({ requestMethod: method })),
 
   handleRequestParamsChange: (params) =>
     set(() => ({ requestUrlParams: params })),
 
   handleRequestOptionChange: (option) => set(() => ({ requestOption: option })),
+
+  handleRequestAuthType: (type) => set(() => ({ authOption: type })),
+
+  handleRequestUsernameData: (data) =>
+    set((state) => ({
+      authData: {
+        ...state.authData,
+        username: data,
+      },
+    })),
+
+  handleRequestPasswordData: (data) =>
+    set((state) => ({
+      authData: {
+        ...state.authData,
+        password: data,
+      },
+    })),
+
+  handleRequestTokenData: (data) =>
+    set((state) => ({
+      authData: {
+        ...state.authData,
+        token: data,
+      },
+    })),
 }));
 
 export default useRequestStore;
