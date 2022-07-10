1917d3019c7baa01c20747543c927941715607b2
unjinjang <alpsboy1996@gmail.com>
2022-07-10 20:34:49 +0900

    [FEAT]: Add logic to update url input when params key value table is added

--- app/features/Request/Url/RequestUrl.js
+++ app/features/Request/Url/RequestUrl.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
+import { PARAMS } from "../../../constants/request";
+import useKeyValueTableStore from "../../../store/useKeyValueTableStore";
 import useRequestStore from "../../../store/useRequestStore";
+import {
+  generateParameterString,
+  removeUrlParameter,
+} from "../../../utils/utils";
 
-function RequestUrl() {
-  const { requestUrl, handleRequestUrlChange } = useRequestStore(
+const RequestUrl = () => {
+  const { requestOption, requestUrl, handleRequestUrlChange } = useRequestStore(
     (state) => state,
   );
+  const { keyValueTableData } = useKeyValueTableStore((state) => state);
+
+  useEffect(() => {
+    if (requestOption !== PARAMS) return;
+
+    const filteredData = keyValueTableData.filter(
+      (data) => data.optionType === PARAMS && data.isChecked,
+    );
+
+    if (filteredData.length === 0) {
+      const parameterRemovedUrl = removeUrlParameter(requestUrl);
+
+      handleRequestUrlChange(parameterRemovedUrl);
+
+      return;
+    }
+
+    const parameterString = generateParameterString(filteredData);
+    const parameterRemovedUrl = removeUrlParameter(requestUrl);
+    const newUrlWithParams = parameterRemovedUrl + parameterString;
+
+    handleRequestUrlChange(newUrlWithParams);
+  }, [keyValueTableData]);
 
   return (
     <InputContainer
@@ -15,7 +44,7 @@ function RequestUrl() {
       onChange={(event) => handleRequestUrlChange(event.target.value)}
     />
   );
-}
+};
 
 const InputContainer = styled.input`
   padding-left: 1.5rem;
