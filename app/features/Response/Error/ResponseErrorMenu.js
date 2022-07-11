5ae1000bf50531ae769b1e6583162d393bdfa3e1
unjinjang <alpsboy1996@gmail.com>
2022-07-12 04:54:27 +0900

    [FEAT]: Add component to display error when http request fails

---
+++ app/features/Response/Error/ResponseErrorMenu.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import errorImage from "../../../assets/image/error.png";
+import Message from "../../../components/Message";
+
+const ResponseErrorMenu = ({ type, message }) => {
+  return (
+    <Message primary>
+      <img src={errorImage} />
+      <h2>Could not send request 😔</h2>
+      <h3>
+        {type}: {message}
+      </h3>
+    </Message>
+  );
+};
+
+ResponseErrorMenu.propTypes = {
+  type: PropTypes.string,
+  message: PropTypes.string,
+};
+
+export default ResponseErrorMenu;
