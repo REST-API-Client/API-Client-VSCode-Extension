edd00eb89530c3a3a56dfd4b0c63b4f051bf2686
unjinjang <alpsboy1996@gmail.com>
2022-07-12 04:52:15 +0900

    [FEAT]: Add constant variables inside response file

--- app/constants/response.js
+++ app/constants/response.js
@@ -1,8 +1,12 @@
 export const TEXT = "text";
+export const ERROR = "Error";
 export const THEME = "vs-dark";
 export const PRETTY = "Pretty";
 export const PREVIEW = "Preview";
 export const RESPONSE = "Response";
+export const META_DATA = "Meta Data";
+export const VIEW_FORMAT = "View Format";
+export const RESPONSE_BODY = "Response Body";
 export const RESPONSE_MENU_OPTIONS = ["Body", "Headers"];
 export const RESPONSE_BODY_OPTIONS = ["Pretty", "Raw", "Preview"];
 export const RESPONSE_RESULT_INFORMATION = ["Status", "Time", "Size"];
