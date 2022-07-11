0c66c70838eb50d11a321f2ff8f9dc1e01719fb5
unjinjang <alpsboy1996@gmail.com>
2022-07-12 04:55:07 +0900

    [FEAT]: Add ResponseMenu component to display http request response

---
+++ app/features/Response/Body/ResponseBodyMenu.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import shallow from "zustand/shallow";
+
+import SelectWrapper from "../../../components/SelectWrapper";
+import {
+  RESPONSE_BODY,
+  RESPONSE_BODY_OPTIONS,
+} from "../../../constants/response";
+import useResponseOptionStore from "../../../store/useResponseOptionStore";
+import ResponseCopyIcon from "../Copy/ResponseCopyIcon";
+import ResponseBodyViewOption from "./ResponseBodyMenuOption";
+
+const RequestBodyMenu = () => {
+  const { responseBodyOption, handleResponseBodyOptionChange } =
+    useResponseOptionStore(
+      (state) => ({
+        responseBodyOption: state.responseBodyOption,
+        handleResponseBodyOptionChange: state.handleResponseBodyOptionChange,
+      }),
+      shallow,
+    );
+
+  const handleOptionChange = (event) => {
+    handleResponseBodyOptionChange(event.target.innerText);
+  };
+
+  return (
+    <SelectWrapper>
+      {RESPONSE_BODY_OPTIONS.map((option, index) => (
+        <OptionContainer
+          key={RESPONSE_BODY + index}
+          primary={responseBodyOption === option}
+          radius={index}
+          onClick={handleOptionChange}
+        >
+          {option}
+        </OptionContainer>
+      ))}
+      <ResponseBodyViewOption />
+      {responseBodyOption !== "Preview" && <ResponseCopyIcon />}
+    </SelectWrapper>
+  );
+};
+
+const OptionContainer = styled.div`
+  padding: 0.6rem 0.9rem;
+  background: ${(props) =>
+    props.primary
+      ? "var(--vscode-button-hoverBackground)"
+      : "rgba(97, 97, 97, 0.15)"};
+  border-radius: ${(props) =>
+    props.radius === 0
+      ? "0.5rem 0 0 0.5rem"
+      : props.radius === 2
+      ? "0 0.5rem 0.5rem 0"
+      : "0"};
+  font-weight: ${(props) => (props.primary ? "400" : "300")};
+  color: var(--default-text);
+  cursor: pointer;
+`;
+
+export default RequestBodyMenu;
