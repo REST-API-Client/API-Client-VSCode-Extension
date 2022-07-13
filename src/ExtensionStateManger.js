8368509fab6b346b4f0410e392a5df94c2b34b57
unjinjang <alpsboy1996@gmail.com>
2022-07-14 03:29:09 +0900

    [FEAT]: Add CRUD logic inside ExtensionStateManager class

--- src/ExtensionStateManger.js
+++ src/ExtensionStateManger.js
@@ -11,10 +11,65 @@ class ExtentionStateManager {
     };
   }
 
-  async updateExtensionContext(state, data) {
-    await this.#context.globalState.update(state, data.history);
+  async addExtensionContext(state, { history }) {
+    await this.#context.globalState.update(state, history);
 
     return;
+  }
+
+  async updateExtensionContext(state, id, status) {
+    this.#context.globalState
+      .get(state)
+      .map(
+        (history) =>
+          history.id === id &&
+          (history.isUserFavorite = !history.isUserFavorite),
+      );
+
+    await this.#context.globalState.update(state, [
+      ...this.#context.globalState.get(state),
+    ]);
+
+    if (status) {
+      const arr = [];
+      const userFavoritesCollection = this.#context.globalState
+        .get(state)
+        .filter((history) => history.isUserFavorite);
+
+      const prevUserFavoriteCollection =
+        this.#context.globalState
+          .get("userFavorites")
+          ?.filter((history) => history.id !== id) || [];
+
+      const totalUserFavoriteCollection = [
+        ...prevUserFavoriteCollection,
+        ...userFavoritesCollection,
+      ];
+
+      const duplicateFilteredUserFavoriteCollection =
+        totalUserFavoriteCollection.filter((history) => {
+          if (!arr.includes(history.id)) {
+            arr.push(history.id);
+            return history;
+          }
+        });
+
+      await this.#context.globalState.update("userFavorites", [
+        ...duplicateFilteredUserFavoriteCollection,
+      ]);
+    }
+
+    return;
+  }
+
+  async deleteExtensionContext(targetExtensionContext, id) {
+    const filteredExtenionContext = this.#context.globalState
+      .get(targetExtensionContext)
+      .filter((history) => history.id !== id);
+
+    await this.#context.globalState.update(targetExtensionContext, [
+      ...filteredExtenionContext,
+    ]);
   }
 }
 
