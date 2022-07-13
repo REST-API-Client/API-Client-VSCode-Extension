d589deeb2e83f15e4bb4d3af9c128a912a9d1aa0
unjinjang <alpsboy1996@gmail.com>
2022-07-13 11:45:32 +0900

    [FEAT]: Add constants variable

--- app/constants/request.js
+++ app/constants/request.js
@@ -2,6 +2,7 @@ export const GET = "Get";
 export const RAW = "Raw";
 export const GZIP = "gzip";
 export const NONE = "None";
+export const TOKEN = "token";
 export const PARAMS = "Params";
 export const ACCEPT = "Accept";
 export const METHOD = "Method";
@@ -9,6 +10,8 @@ export const REQUEST = "Request";
 export const DEFLATE = "deflate";
 export const NO_AUTH = "No Auth";
 export const ANY_MIME_TYPE = "*/*";
+export const PASSWORD = "password";
+export const USERNAME = "username";
 export const NO_CACHE = "no-cache";
 export const AUTH = "Authorization";
 export const FORM_DATA = "Form Data";
@@ -29,7 +32,15 @@ export const REQUEST_MENU_OPTIONS = [
   "Body",
   "Options",
 ];
-
+export const REQUEST_BODY_RAW_OPTIONS = [
+  { option: "Text", headerField: "text/plain" },
+  { option: "JavaScript", headerField: "application/javascript" },
+  {
+    option: "JSON",
+    headerField: "application/json",
+  },
+  { option: "HTML", headerField: "text/html" },
+];
 export const REQUEST_BODY_OPTIONS = [
   { option: "None", headerField: "" },
   { option: "Form Data", headerField: "multipart/form-data" },
@@ -37,5 +48,5 @@ export const REQUEST_BODY_OPTIONS = [
     option: "x-www-form-urlencoded",
     headerField: "application/x-www-form-urlencoded",
   },
-  { option: "Raw", headerField: "application/json" },
+  { option: "Raw", headerField: "text/plain" },
 ];
