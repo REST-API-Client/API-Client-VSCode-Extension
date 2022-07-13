871a825f050989727e57b37f000de3d9d3946fcd
unjinjang <alpsboy1996@gmail.com>
2022-07-13 16:35:28 +0900

    [FEAT]: Add component to display when sidebar collections are empty

---
+++ webview/features/Sidebar/Empty/EmptyFavoritesMessage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Message from "../../../components/Message";
+
+const EmptyFavoritesMessage = () => {
+  return (
+    <Message>
+      <AiOutlineHeart className="sidebarEmptyIcon" />
+      <p>Your favorites collection seems to be empty.</p>
+      <p>
+        Press the heart icon from your history collection to add it to your
+        favorites collection
+      </p>
+    </Message>
+  );
+};
+
+export default EmptyFavoritesMessage;
