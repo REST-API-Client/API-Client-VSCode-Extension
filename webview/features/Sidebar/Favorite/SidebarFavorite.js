fd26267341c3e67358d6a21e932b9dacefb66376
unjinjang <alpsboy1996@gmail.com>
2022-07-14 03:20:44 +0900

    [REMOVE]: Delete files and make it into one shared component

--- webview/features/Sidebar/Favorite/SidebarFavorite.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import EmptyFavoritesMessage from "../Empty/EmptyFavoritesMessage";
-
-const SidebarFavorites = () => {
-  return <EmptyFavoritesMessage />;
-};
-
-export default SidebarFavorites;
