87892864005893cf9d3bd6abb1fa88dbd12efcd3
unjinjang <alpsboy1996@gmail.com>
2022-07-14 17:40:45 +0900

    [FEAT]: Add constants index.js to expose all constants file object

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export { default as CATEGORY } from "./category";
+export { default as COLLECTION } from "./collection";
+export { default as COMMAND } from "./command";
+export { default as MESSAGE } from "./message";
+export { default as NAME } from "./name";
+export { default as TYPE } from "./types";
