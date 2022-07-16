02442fc3930f4a04f5826ac844687c10e619524b
unjinjang <alpsboy1996@gmail.com>
2022-07-16 21:49:22 +0900

    [FEAT]: Add feature to search and filter specific collection url

--- webview/shared/SidebarCollection.js
+++ webview/shared/SidebarCollection.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { FaTrashAlt } from "react-icons/fa";
 import styled from "styled-components";
@@ -7,7 +7,9 @@ import styled from "styled-components";
 import Information from "../components/Information";
 import MoreInformation from "../components/MoreInformation";
 import { SIDEBAR } from "../constants";
-import EmptyCollectionMessage from "../features/Sidebar/Empty/EmptyCollectionMessage";
+import SidebarDeleteAllButton from "../features/Sidebar/Button/SidebarDeleteAllButton";
+import EmptyCollectionMenu from "../features/Sidebar/Menu/EmptyCollectionMenu";
+import EmptySearchResultMessage from "../features/Sidebar/Message/EmptySearchResultMessage";
 import { calculateCollectionTime, generateMethodColor } from "../utils";
 
 const SidebarCollection = ({
@@ -18,122 +20,139 @@ const SidebarCollection = ({
   handleDeleteAllButton,
   handleSidebarFavoriteIcon,
 }) => {
-  return (
-    <CollectionWrapper>
-      {userCollection?.length ? (
-        <div
-          className="deleteAllButtonContainer"
-          onClick={handleDeleteAllButton}
-        >
-          <h5>Delete all</h5>
-        </div>
-      ) : null}
-      {userCollection?.length ? (
-        userCollection.map(
-          ({
-            url,
-            method,
-            isUserFavorite,
-            id,
-            requestedTime,
-            favoritedTime,
-          }) => {
-            const methodColor = generateMethodColor(method.toLowerCase());
-            const collectionCreatedTime =
-              calculateCollectionTime(requestedTime);
-            const favoriteListedTime = calculateCollectionTime(favoritedTime);
+  const [searchInputValue, setSearchInputValue] = useState("");
 
-            return (
-              <HistoryListWrapper key={sidebarOption + id}>
-                <Information textColor={methodColor}>
-                  <div className="methodContainer">
-                    <h4>{method}</h4>
-                  </div>
-                  <div>
-                    <p onClick={() => handleUrlClick(id)}>{url}</p>
-                  </div>
-                </Information>
-                <MoreInformation>
-                  <div>
-                    {sidebarOption === SIDEBAR.HISTORY ? (
-                      <p>{collectionCreatedTime}</p>
-                    ) : (
-                      <p>Added {favoriteListedTime}</p>
-                    )}
-                  </div>
-                  <div>
-                    {sidebarOption === SIDEBAR.HISTORY ? (
-                      isUserFavorite ? (
-                        <AiFillHeart
-                          className="sidebarIcon favorite"
-                          onClick={() =>
-                            handleSidebarFavoriteIcon(
-                              SIDEBAR.REMOVE_FROM_FAVORITES,
-                              id,
-                            )
-                          }
-                        />
-                      ) : (
-                        <AiOutlineHeart
-                          className="sidebarIcon"
-                          onClick={() =>
-                            handleSidebarFavoriteIcon(
-                              SIDEBAR.ADD_TO_FAVORITES,
-                              id,
-                            )
-                          }
-                        />
-                      )
-                    ) : null}
-                    <FaTrashAlt
-                      className="sidebarIcon"
-                      onClick={() => handleDeleteButton(id)}
-                    />
-                  </div>
-                </MoreInformation>
-              </HistoryListWrapper>
-            );
-          },
-        )
-      ) : (
-        <EmptyCollectionMessage currentSidebarOption={sidebarOption} />
-      )}
-    </CollectionWrapper>
+  return (
+    <>
+      {userCollection?.length ? (
+        <UtilitySectionWrapper>
+          <input
+            type="text"
+            placeholder="Search collection..."
+            value={searchInputValue}
+            onChange={(event) => setSearchInputValue(event.target.value)}
+          />
+        </UtilitySectionWrapper>
+      ) : null}
+      <CollectionWrapper>
+        {userCollection?.length ? (
+          userCollection.filter((collection) =>
+            collection.url.toLowerCase().includes(searchInputValue),
+          ).length !== 0 ? (
+            <>
+              <SidebarDeleteAllButton clickHandler={handleDeleteAllButton} />
+              <CollectionDetailWrapper>
+                {userCollection.map(
+                  ({
+                    url,
+                    method,
+                    isUserFavorite,
+                    id,
+                    requestedTime,
+                    favoritedTime,
+                  }) => {
+                    const methodColor = generateMethodColor(
+                      method.toLowerCase(),
+                    );
+                    const collectionCreatedTime =
+                      calculateCollectionTime(requestedTime);
+                    const favoriteListedTime =
+                      calculateCollectionTime(favoritedTime);
+
+                    return (
+                      <HistoryListWrapper key={sidebarOption + id}>
+                        <Information textColor={methodColor}>
+                          <div className="methodContainer">
+                            <h4>{method}</h4>
+                          </div>
+                          <div>
+                            <p onClick={() => handleUrlClick(id)}>{url}</p>
+                          </div>
+                        </Information>
+                        <MoreInformation>
+                          <div>
+                            {sidebarOption === SIDEBAR.HISTORY ? (
+                              <p>{collectionCreatedTime}</p>
+                            ) : (
+                              <p>Added {favoriteListedTime}</p>
+                            )}
+                          </div>
+                          <div>
+                            {sidebarOption === SIDEBAR.HISTORY ? (
+                              isUserFavorite ? (
+                                <AiFillHeart
+                                  className="sidebarIcon favorite"
+                                  onClick={() =>
+                                    handleSidebarFavoriteIcon(
+                                      SIDEBAR.REMOVE_FROM_FAVORITES,
+                                      id,
+                                    )
+                                  }
+                                />
+                              ) : (
+                                <AiOutlineHeart
+                                  className="sidebarIcon"
+                                  onClick={() =>
+                                    handleSidebarFavoriteIcon(
+                                      SIDEBAR.ADD_TO_FAVORITES,
+                                      id,
+                                    )
+                                  }
+                                />
+                              )
+                            ) : null}
+                            <FaTrashAlt
+                              className="sidebarIcon"
+                              onClick={() => handleDeleteButton(id)}
+                            />
+                          </div>
+                        </MoreInformation>
+                      </HistoryListWrapper>
+                    );
+                  },
+                )}
+              </CollectionDetailWrapper>
+            </>
+          ) : (
+            <EmptySearchResultMessage value={searchInputValue} />
+          )
+        ) : (
+          <EmptyCollectionMenu currentSidebarOption={sidebarOption} />
+        )}
+      </CollectionWrapper>
+    </>
   );
 };
 
 const CollectionWrapper = styled.div`
+  width: 100%;
+`;
+
+const CollectionDetailWrapper = styled.div`
   overflow-y: scroll;
-  height: 70vh;
+  height: 62vh;
+`;
 
-  .deleteAllButtonContainer {
-    display: flex;
-    justify-content: flex-end;
-    margin: 1rem 0 0.25rem 0;
-  }
-
-  h5 {
-    text-align: end;
-    padding: 0.25rem 0.4rem;
-    color: #cbcbcb;
-    border-radius: 10px;
-    background: #853333;
-    transition: opacity 0.15s ease-in-out;
-    cursor: pointer;
-
-    :hover {
-      opacity: 0.45;
-    }
+const UtilitySectionWrapper = styled.div`
+  input {
+    margin-top: 1rem;
+    padding: 0.4rem 0.6rem;
+    border-radius: 0.35rem;
+    color: white;
   }
 `;
 
 const HistoryListWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 0.35rem;
-  padding: 0.55rem 0 0 0.25rem;
+  margin: 0.35rem 0.15rem 0.5rem 0;
+  padding: 0.55rem 0.1rem 0 0.25rem;
   border-bottom: 0.07rem solid #404040;
   overflow: hidden;
+
+  p {
+    font-size: 0.92rem;
+  }
 `;
 
 SidebarCollection.propTypes = {
