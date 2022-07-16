b318e92d1e7b2bcafe1271d9fd135574f3dfad49
unjinjang <alpsboy1996@gmail.com>
2022-07-17 04:27:36 +0900

    [REFACTOR]: Change styled component wrapper name

--- webview/features/Request/Method/RequestMethod.js
+++ webview/features/Request/Method/RequestMethod.js
@@ -10,20 +10,20 @@ const RequestMethod = () => {
   );
 
   return (
-    <MethodOptionWrapper
-      onChange={(event) => handleRequestMethodChange(event.target.value)}
+    <MethodSelectOptionWrapper
       name="httpRequestMethods"
+      onChange={(event) => handleRequestMethodChange(event.target.value)}
     >
       {OPTION.REQUEST_METHOD_OPTIONS.map((requestMethod, index) => (
         <option key={REQUEST.METHOD + index} value={requestMethod}>
           {requestMethod}
         </option>
       ))}
-    </MethodOptionWrapper>
+    </MethodSelectOptionWrapper>
   );
 };
 
-const MethodOptionWrapper = styled.select`
+const MethodSelectOptionWrapper = styled.select`
   width: 9rem;
   height: 3.15rem;
   padding-left: 1.2rem;
